3801b393db8092f4d48916c7b7ab835300fbc25a
Vu Duy Long <31239859+d2fzero@users.noreply.github.com>
2017-09-21 11:16:12 +0700

    Add files via upload

---
+++ Assets/scrips/gamecontroller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gamecontroller : MonoBehaviour {
+	public Text hintText;
+	public InputField inputField;
+	public Text levelText;
+
+
+	// normal val
+	public string levelAnswer;
+	public string levelNumber;
+	public string levelHint;
+	public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+		hintText.text = levelHint;
+		
+	}
+	public void CheckAnswer(){
+		if (inputField.text == levelAnswer) {
+			hintText.text = "yay";
+			hintText.color = Color.green;
+			SceneManager.LoadScene (nextScene);
+		} else {
+			hintText.text = "access denied";
+			hintText.color = Color.red;
+			inputField.text = "";
+			inputField.ActivateInputField ();
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
